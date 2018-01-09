4aedc99486cfe34724604a2bf0d6d50e4cc18baf
bowen-wu <13793248162@163.com>
2018-01-09 19:32:03 +0800

    添加自动播放功能

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ $(() => {
     $index.each((index,element) => {
         $index.eq(index).on('click',() => {
             $index.eq(index).addClass('active').siblings().removeClass('active')
+            // 此处也可以使用 margin-left  .css({'margin-left': index*$picWidth+'px'})
             $carousel.eq(0).css({transform:'translateX(-'+ index*$picWidth+'px)'})
         })
     }).eq(0).addClass('active')
+
+    let timer = null
+    let index = 0
+    timer = setInterval(() => {
+        // 如果用 if 判断，那么就需要使用 $index.eq(index).triggerHandler('click')
+        // if(index >= $index.length){
+        //     index = 0
+        // }
+        index++
+        $index.eq(index%$index.length).triggerHandler('click')
+    },2500)
+
 })
