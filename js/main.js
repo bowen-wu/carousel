d74d380fd962e064f3da4679192316a8fb5dc59e
bowen-wu <13793248162@163.com>
2018-01-09 19:56:35 +0800

    优化代码

--- js/main.js
+++ js/main.js
@@ -2,23 +2,34 @@ $(() => {
     let $index = $('#index > ul > li')
     let $carousel = $('.carousel')
     let $picWidth = $carousel.children().eq(0).width()
-    $index.each((index,element) => {
-        $index.eq(index).on('click',() => {
-            $index.eq(index).addClass('active').siblings().removeClass('active')
-            // 此处也可以使用 margin-left  .css({'margin-left': index*$picWidth+'px'})
-            $carousel.eq(0).css({transform:'translateX(-'+ index*$picWidth+'px)'})
+    let index = 0
+    $index.each((indexInner,element) => {
+        $index.eq(indexInner).on('click',() => {
+            $index.eq(indexInner).addClass('active').siblings().removeClass('active')
+            // 此处也可以使用 margin-left  .css({'margin-left': indexInner*$picWidth+'px'})
+            $carousel.eq(0).css({transform:'translateX(-'+ indexInner*$picWidth+'px)'})
+            index = indexInner
         })
     }).eq(0).addClass('active')
 
     let timer = null
-    let index = 0
-    timer = setInterval(() => {
-        // 如果用 if 判断，那么就需要使用 $index.eq(index).triggerHandler('click')
-        // if(index >= $index.length){
-        //     index = 0
-        // }
-        index++
-        $index.eq(index%$index.length).triggerHandler('click')
-    },2500)
+    timer = setTimer()
+
+    $carousel.hover(() => {
+        clearInterval(timer)
+    }, () => {
+        timer = setTimer()
+    })
+
+    function setTimer() {
+        return setInterval(() => {
+            // 如果用 if 判断，那么就需要使用 $index.eq(index).triggerHandler('click')
+            // if(index >= $index.length){
+            //     index = 0
+            // }
+            index++
+            $index.eq(index % $index.length).triggerHandler('click')
+        }, 2000)
+    }
 
 })
