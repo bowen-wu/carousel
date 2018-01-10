7d892a9539aed25f90b7a74afbcf91bbff547678
bowen-wu <13793248162@163.com>
2018-01-10 12:46:32 +0800

    重构

--- js/main.js
+++ js/main.js
@@ -1,31 +1,28 @@
 $(() => {
     let $index = $('#index > ul > li')
-    let $carousel = $('.carousel')
-    let $picWidth = $carousel.children().eq(0).width()
+    let $wrapper = $('#window > .wrapper')
+    let $lis = $wrapper.children()
+    let $picWidth = $lis.eq(0).width()
     let index = 0
     $index.each((indexInner,element) => {
         $index.eq(indexInner).on('click',() => {
             $index.eq(indexInner).addClass('active').siblings().removeClass('active')
-            // 此处也可以使用 margin-left  .css({'margin-left': indexInner*$picWidth+'px'})
-            $carousel.eq(0).css({transform:'translateX(-'+ indexInner*$picWidth+'px)'})
+            $wrapper.css({transform: 'translate(-' + $picWidth * indexInner + 'px)'})
             index = indexInner
         })
-    }).eq(0).addClass('active')
-    let timer = null
-    timer = autoPlay()
-    $carousel.hover(() => {
+    }).eq(0).triggerHandler('click')
+
+    let timer = autoPlay()
+    $wrapper.hover(() => {
         clearInterval(timer)
-    }, () => {
+    },() => {
         timer = autoPlay()
     })
+
     function autoPlay() {
         return setInterval(() => {
-            // 如果用 if 判断，那么就需要使用 $index.eq(index).triggerHandler('click')
-            // if(index >= $index.length){
-            //     index = 0
-            // }
             index++
-            $index.eq(index % $index.length).triggerHandler('click')
-        }, 2000)
+            $index.eq(index % $lis.length).triggerHandler('click')
+        }, 2500)
     }
 })
