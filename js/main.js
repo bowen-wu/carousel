153fa43844dff1d1d94cff1b23e7dbf548bc61ca
bowen-wu <13793248162@163.com>
2018-01-13 11:39:01 +0800

    优化代码

--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
 $(() => {
     let $index = $('#index > ul > li')
-    let $wrapper = $('#window > .wrapper')
+    let $wrapper = $('#window > #wrapper')
     let $lis = $wrapper.children()
     let $picWidth = $lis.eq(0).width()
     let index = 0
+
     clickEvent()
-
     let timer = autoPlay()
-    $wrapper.hover(() => {
-        clearInterval(timer)
-    },() => {
-        timer = autoPlay()
-    })
-    $(document).on('visibilitychange',() => {
-        console.log(document.hidden)
-        if(document.hidden){
-            clearInterval(timer)
-        }else{
-            timer = autoPlay()
-        }
-    })
-
+    hoverAndPage()
+   
     // 工具函数
     function clickEvent() {
         $index.each((indexInner, element) => {
@@ -35,10 +23,22 @@ $(() => {
     }
     function autoPlay() {
         return setInterval(() => {
-            console.log(new Date)
             index++
             $index.eq(index % $lis.length).triggerHandler('click')
         }, 2500)
     }
-    
+    function hoverAndPage(){
+        $wrapper.hover(() => {
+            clearInterval(timer)
+        },() => {
+            timer = autoPlay()
+        })
+        $(document).on('visibilitychange',() => {
+            if(document.hidden){
+                clearInterval(timer)
+            }else{
+                timer = autoPlay()
+            }
+        })
+    }
 })
